bf8b051c6afd679f4617339153398a62b145c3d5
RoryMack <99274597+RoryMack2@users.noreply.github.com>
2022-03-22 00:18:29 +0000

    added js button

---
+++ js/script.js
@@ -0,0 +1,11 @@
+// Copyright (c) 2022 Mackay, Rory All rights reserved
+//
+// Created by: Mackay, Rory
+// Created on: Mar 2022
+// This file contains the JS functions for index.html
+
+function myButtonClicked () {
+  // this function displays "Hello, World!"
+  
+  document.getElementById('hello-world').innerHTML = 'BOOM'
+}
